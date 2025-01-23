dc03740072bcf7494715fb7d7eb9478a29cd8e36
Daryna <buriak.daryna@gmail.com>
2025-01-23 14:17:44 +0200

    bugfixing

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -6,7 +6,7 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 export const createGalleryCardTemplate = imgInfo => {
   return `
     <li class="gallery-card">
-      <a class="gallery-link  href="${imgInfo.largeImageURL}">
+      <a class="gallery-link"  href="${imgInfo.largeImageURL}">
         <img class="gallery-img" src="${imgInfo.webformatURL}" alt="${imgInfo.tags}" /> 
         <ul class="text-info">
           <li class="image-info">
